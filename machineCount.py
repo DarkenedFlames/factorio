3461032c7ae4c3ed86674d2d3a2f26707069b594
DarkenedFlames <110654575+DarkenedFlames@users.noreply.github.com>
2024-10-05 23:16:30 -0700

    Update machineCount.py
    Made more efficient

--- machineCount.py
+++ machineCount.py
@@ -10,29 +10,22 @@ batch = np.array(batch)
 speed = np.array(speed)
 
 
-
 def between(number, lowerLimit, upperLimit):
-    if number > lowerLimit and number < upperLimit:
-        return True
-    return False
+    return lowerLimit < number < upperLimit
 
 def listLCD(floats):
-    floats_list = [float(f) for f in np.array(floats).flatten()]
-    denominators = [Fraction(f).limit_denominator().denominator for f in floats_list]
+    denominators = [Fraction(float(f)).limit_denominator().denominator for f in floats]
     return np.lcm.reduce(denominators)
 
 def machineCount(product, tier):
     r = speed[tier-1]*batch/time
-    R_inv = np.linalg.inv(np.array(counts))
+    R_inv = np.linalg.inv(counts)
 
     o = item.index(product)
     w = r[o]*R_inv[o]/r
     M = (w * listLCD(w)).tolist()
-    output = defaultdict(list)
-    for x in range(0,len(M)):
-        if not between(M[x],-1,1):
-            output[item[x]] = round(M[x],3)
+
+    output = {item[x]: round(M[x], 3) for x in range(len(M)) if not between(M[x], -1, 1)}
     return output
 
 print(machineCount("electronic_circuit",3))
-
